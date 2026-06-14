3353b661645f53686496dc01bec9a0ad32c08c18
Alirezapygo <arazmi377@gmail.com>
2026-06-14 19:45:13 +0330

    added general templates folder
    move website templates to general folder of templates

--- person/views.py
+++ person/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def abut(request):
-    return render(request,'websit/abut.html')
+    return render(request,'website/abut.html')
 def shop(request):
-    return render(request, 'websit/shop.html')
+    return render(request, 'website/shop.html')
 def home(request):
-    return render(request,'websit/index.html')
+    return render(request,'website/index.html')
